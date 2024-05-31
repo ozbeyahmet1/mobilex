6837746d124954bffc05c9c425556e0106d882f6
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-05-31 03:30:06 +0300

    config: adds tailwind configurations for global styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue:"#0381FF",
+      }
+    },
+    screens: {
+      'sm': '0',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+      '3xl': '1600px',
+    },
   },
   plugins: [],
 }
